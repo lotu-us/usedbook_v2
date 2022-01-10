25702814ae20e2f6610ad4a95f65c122bceb009f
thwjd <thwjd1356@naver.com>
2022-01-11 01:07:53 +0900

    대시보드 페이지 추가

---
+++ src/main/java/team/hello/usedbook/controller/DashboardController.java
@@ -0,0 +1,30 @@
+package team.hello.usedbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import team.hello.usedbook.config.SessionConstants;
+import team.hello.usedbook.domain.Member;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard/myInfo")
+    public String myInfo(HttpSession session, Model model){
+        Member loginMember = (Member) session.getAttribute(SessionConstants.LOGIN_MEMBER);
+        model.addAttribute("loginMember", loginMember);
+        return "dashboard/myInfo";
+    }
+
+    @GetMapping("/dashboard/myPosts")
+    public String myPosts(){
+        return "dashboard/myPosts";
+    }
+
+    @GetMapping("/dashboard/myComments")
+    public String myComments(){
+        return "dashboard/myComments";
+    }
+}
